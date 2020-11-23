97b46ddea460fc9daffc1bbc201f6d7d931a672b
GalacticGlum <nerftactic09@gmail.com>
2020-11-23 13:34:16 -0500

    Modified scripts/process_data.py

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -29,12 +29,16 @@ use additional hard disk space for processing the data. If processing
 large amounts of data (> 100 GB), at least 8 GB of RAM is recommended.
 """
 
+
 import bz2
 import json
 import argparse
 from tqdm import tqdm
 from typing import Set
 from pathlib import Path
+from texttable import Texttable
+from hurry.filesize import size
+from dataclasses import dataclass
 from concurrent.futures import (
     ThreadPoolExecutor,
     ProcessPoolExecutor,
@@ -44,14 +48,19 @@ from concurrent.futures import (
 
 parser = argparse.ArgumentParser(description='Process raw Twitter data.')
 parser.add_argument('input_directory', type=Path, help='Directory containing the data.')
+parser.add_argument('-o', '--destination-directory', type=Path, default=None,
+                    help='The directory to save the files. If unspecified, '
+                    'defaults to the location of the processed bz2 file.')
 parser.add_argument('-d', '--delete-source', action='store_true', dest='delete_source',
                     help='Whether to delete the source .bz2 files after processing.')
 parser.add_argument('-l', '--language-filters', nargs='+', type=str, default=['en'],
                     help='Languages to include. If empty, no language filtering is applied.')
-parser.add_argument('--keep-retweets', action='store_false', dest='keep_retweets',
+parser.add_argument('--keep-retweets', action='store_true', dest='keep_retweets',
                     help='Whether to keep retweet (tweets that have a "retweeted_status" param).')
 parser.add_argument('--glob', type=str, default='**/*.bz2', help='Glob pattern to find data files.')
 parser.add_argument('--num-workers', type=int, default=8, help='The number of threads to use. Defaults to 8.')
+parser.add_argument('--summarise', action='store_true', dest='summarise',
+                    help='Display a summary of the data processing.')
 parser.set_defaults(keep_retweets=False)
 
 
@@ -65,7 +74,6 @@ def clean_tweet(tweet_data: dict) -> dict:
         - 'id' in tweet_data
         - 'text' in tweet_data
     """
-    if 'id' not in tweet_data: print(tweet_data)
     result = {
         'id': tweet_data['id'],
         'text': tweet_data['text'],
@@ -93,9 +101,8 @@ def is_retweet(tweet_data: dict) -> bool:
     return tweet_data.get('retweeted_status', None) is not None
 
 
-def keep_tweet(tweet_data: dict, language_filters: Set[str], keep_retweets=False) \
-    -> bool:
-    """Return whether to keep the tweet.
+def is_valid_tweet(tweet_data: dict, language_filters: Set[str]) -> bool:
+    """Return whether the tweet is valid.
 
     Preconditions:
         - language_filters is not None
@@ -106,18 +113,37 @@ def keep_tweet(tweet_data: dict, language_filters: Set[str], keep_retweets=False
         'text' not in tweet_data:
         return False
 
-    if not keep_retweets and is_retweet(tweet_data):
-        return False
+    return len(language_filters) == 0 or tweet_data.get('lang', None) in language_filters
 
-    return tweet_data.get('lang', None) not in language_filters
+
+@dataclass
+class ProcessedStatistics:
+    """Statistics about a processed file.
+
+    Instance Attributes:
+        - source_size: the size of the source file in bytes.
+        - processed_size: the size of the processed file in bytes.
+        - num_tweets_source: the number of tweets in the source file.
+        - num_tweets_processed: the number of tweets after cleaning/filtering.
+    """
+    source_size: int
+    processed_size: int
+    num_tweets_source: int
+    num_tweets_processed: int
 
 
 def process_file(filepath: Path, language_filters: Set[str]=None, \
-                 keep_retweets=False, delete_source=False) -> None:
-    """Process a .bz2 file based on the given language filters."""
+                 keep_retweets: bool=False, delete_source: bool=False, \
+                 destination_directory: Path=None) -> ProcessedStatistics:
+    """Process a .bz2 file based on the given language filters.
+
+    Return a ProcessedStatistics object.
+    """
     with open(filepath, 'rb') as file:
         # Decompress file and load it into memory
         data = bz2.decompress(file.read())
+        source_size_decompressed = len(data)
+
         # Convert the bytearray to a string
         data = data.decode('utf-8').strip()
 
@@ -142,25 +168,48 @@ def process_file(filepath: Path, language_filters: Set[str]=None, \
 
     # Default to empty set if the passed in value was None
     language_filters = language_filters or set()
-    # Perform language filtering and cleaning
-    tweets = [
-        clean_tweet(tweet) for tweet in tweets
-        if keep_tweet(tweet, language_filters, keep_retweets)
-    ]
+    cleaned_tweets = []
+    for tweet in tweets:
+        if not is_valid_tweet(tweet, language_filters):
+            continue
+
+        retweet = is_retweet(tweet)
+        if retweet:
+            # For retweets, we get the original tweet.
+            orignal_tweet = clean_tweet(tweet['retweeted_status'])
+            cleaned_tweets.append(orignal_tweet)
+
+        if not retweet or retweet and keep_retweets:
+            cleaned_tweets.append(clean_tweet(tweet))
 
     if delete_source:
-        # This is a risky and irreversible operation!
-        # We delete the source before saving the cleaned tweets
-        # to ideally free up space for the output file. But,
-        # this means that if there was an error, the source is gone.
-        filepath.unlink()
+        try:
+            # This is a risky and irreversible operation!
+            # We delete the source before saving the cleaned tweets
+            # to ideally free up space for the output file. But,
+            # this means that if there was an error, the source is gone.
+            filepath.unlink()
+        except:
+            # TODO: log this!
+            pass
 
     destination_filepath = filepath.with_suffix('')
     if destination_filepath.suffix != '.json':
         destination_filepath.with_suffix('.json')
 
+    if destination_directory is not None:
+        destination_directory.mkdir(exist_ok=True, parents=True)
+        destination_filepath = destination_directory / destination_filepath.name
+
     with open(destination_filepath, 'w+') as output_file:
-        json.dump(tweets, output_file)
+        json.dump(cleaned_tweets, output_file)
+
+    return ProcessedStatistics(
+        source_size_decompressed, # Size of source in bytes
+        destination_filepath.stat().st_size, # Size of processed in bytes
+        len(tweets), # number of tweets in the source file
+        len(cleaned_tweets) # number of tweets after filtering/cleaning
+    )
 
 
 def main(args: argparse.Namespace) -> None:
@@ -173,19 +222,38 @@ def main(args: argparse.Namespace) -> None:
     process_file_kwargs = {
         'language_filters': language_filters,
         'keep_retweets': args.keep_retweets,
-        'delete_source': args.delete_source
+        'delete_source': args.delete_source,
+        'destination_directory': args.destination_directory
     }
 
-    parallel_map(
+    # sizes is a list of ProcessedStatistics objects.
+    sizes = parallel_map(
         [{'filepath': file, **process_file_kwargs} for file in files],
         process_file,
         n_jobs=args.num_workers,
         use_kwargs=True
     )
 
+    if args.summarise:
+        total_source_size = sum(stats.source_size for stats in sizes)
+        total_processed_size = sum(stats.processed_size for stats in sizes)
+        total_source_tweets = sum(stats.num_tweets_source for stats in sizes)
+        total_processed_tweets = sum(stats.num_tweets_processed for stats in sizes)
 
-def parallel_map(array, function, n_jobs=16, use_kwargs=False, front_num=3, multithread=False,
-                 show_progress_bar=True, extend_result=False, initial_value=list()):
+        print(f'Processed a total of {len(sizes)} files.\n')
+        table = Texttable()
+        table.set_deco(Texttable.HEADER)
+        table.add_rows(
+            [['', 'raw', 'cleaned'],
+             ['total size', size(total_source_size), size(total_processed_size)],
+             ['total tweets', total_source_size, total_processed_tweets]]
+        )
+        print(table.draw())
+
+
+def parallel_map(array: list, function: callable, n_jobs: int=16, use_kwargs: bool=False,
+                 front_num: int=3, multithread: bool=False, show_progress_bar: bool=True,
+                 extend_result: bool=False, initial_value: list=list()):
     """
     A parallel version of the map function with a progress bar.
     :note:
