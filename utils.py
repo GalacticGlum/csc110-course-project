98c560eb45c9c81a4a940f9a7293767b177d65e1
GalacticGlum <nerftactic09@gmail.com>
2020-12-03 22:40:04 -0500

    Updated parallel_map to allow for custom append/extend functionality

--- utils.py
+++ utils.py
@@ -9,8 +9,8 @@ def parallel_map(iterables: Union[list, iter], function: callable, n_jobs: Optio
                  use_kwargs: Optional[bool] = False, front_num: Optional[int] = 3,
                  show_progress_bar: Optional[bool] = True, initial_value: Optional[list] = None,
                  raise_errors: Optional[bool] = False, include_errors: Optional[bool] = True,
-                 extend_result: Optional[bool] = False, return_output: Optional[bool] = True) \
-        -> Union[list, None]:
+                 extend_result: Optional[bool] = False, return_output: Optional[bool] = True,
+                 add_func: Optional[callable] = None) -> Union[list, None]:
     """A parallel version of the map function with a progress bar.
     Return a list of the form [function(iterables[0]), function(iterables[1]), ...].
 
@@ -30,6 +30,9 @@ def parallel_map(iterables: Union[list, iter], function: callable, n_jobs: Optio
         extend_result: Whether the resultant list should be extended rather than appended to.
         return_output: Whether to return a list containing the output values of the function.
             If False, this function does not return None.
+        add_func: A custom function for adding the output values of the function to the result list.
+            This function has two parameters, the value to add and the list to add it to, and it
+            should mutate the list.
 
     Preconditions:
         - n_jobs >= 1
@@ -44,12 +47,29 @@ def parallel_map(iterables: Union[list, iter], function: callable, n_jobs: Optio
             a = next(iterables)
             front.append(function(**a) if use_kwargs else function(a))
 
+    def _add_func(x: object, output: list) -> None:
+        """Add a value to the output list."""
+        # No reason to add if we aren't returning the output.
+        if not return_output:
+            return
+
+        if add_func is not None:
+            add_func(x, output)
+        else:
+            if extend_result:
+                output.extend(x)
+            else:
+                output.append(x)
+
+    output = initial_value or list()
+    for x in front:
+        _add_func(x, output)
+
     # If n_jobs == 1, then we are not parallelising, run all elements serially.
     if n_jobs == 1:
-        output = front + [
-            function(**a) if use_kwargs else function(a)
-            for a in tqdm(iterables)
-        ]
+        for a in tqdm(iterables):
+            x = function(**a) if use_kwargs else function(a)
+            _add_func(x, output)
 
         return output if return_output else None
 
@@ -68,18 +88,14 @@ def parallel_map(iterables: Union[list, iter], function: callable, n_jobs: Optio
     if not return_output:
         return None
 
-    output = initial_value or list()
-    output.extend(front)
-
-    _add_func = lambda x: output.extend(x) if extend_result else output.append(x)
     for _, future in tqdm(enumerate(futures)):
         try:
-            _add_func(future.result())
+            _add_func(future.result(), output)
         except Exception as exception:
             if raise_errors:
                 raise exception
             if include_errors:
-                _add_func(exception)
+                _add_func(exception, output)
 
     return output
 
