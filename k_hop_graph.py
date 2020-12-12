0c1c04f4ec9516fa9a9ad90075a0c45627ab8a8a
GalacticGlum <nerftactic09@gmail.com>
2020-12-12 17:53:54 -0500

    Working k-hop graph renderer

--- k_hop_graph.py
+++ k_hop_graph.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 import community as community_louvain
 from matplotlib.collections import LineCollection
 
-from sklearn import metrics
 from fa2 import ForceAtlas2
+from sklearn import metrics
 from curved_edges import curved_edges
 from word_embeddings import WordEmbeddings
 
@@ -36,7 +36,8 @@ def build_infinity_hop_graph(embeddings: WordEmbeddings, alpha: Optional[float] 
     for pair in tqdm.tqdm(pairs):
         i, j = pair
         if i == j: continue
-        graph.add_edge(i, j)
+        # The weight of the edge is the cosine similary.
+        graph.add_edge(i, j, weight=similarities[i][j])
 
     return graph
 
@@ -77,42 +78,93 @@ def build_k_hop_graph(embeddings: WordEmbeddings, target_word: str,
     return graph
 
 
-# G = nx.random_geometric_graph(400, 0.2)
+def draw_k_hop_graph(embeddings: WordEmbeddings, target_word: str,
+                     k: int, alpha: Optional[float] = 0.50,
+                     min_node_size: Optional[float] = 20,
+                     max_node_size: Optional[float] = 120,
+                     min_font_size: Optional[float] = 6,
+                     max_font_size: Optional[float] = 24) -> None:
+    """Draw the k-hop graph for the given word embeddings and interest word.
+
+    Args:
+        embeddings: The word embeddings to generate the graph for.
+        target_word: The word of interest.
+        k: The number of 'hops' between the word of interest and every node
+            in the graph. The resultant graph has the property that the word
+            of interest is reachable from any node in at most k edges.
+        alpha: The similarity threshold. Words that have a cosine similarity
+            of at least this threshold are kept, and the rest are discarded.
+        min_node_size: The minimum size of a node, in pixels.
+        max_node_size: The maximum size of a node, in pixels.
+        min_font_size: The minimum size of a label, in pixels.
+        max_font_size: The maximum size of a label, in pixels.
+    """
+    graph = build_k_hop_graph(embeddings, target_word, k, alpha=alpha)
+
+    print('computing best partition')
+    # compute the best partition
+    partition = community_louvain.best_partition(graph)
+
+    print('computing layout')
+    forceatlas2 = ForceAtlas2(
+        outboundAttractionDistribution=True,
+        edgeWeightInfluence=1.0,
+        jitterTolerance=1.0,
+        barnesHutOptimize=True,
+        barnesHutTheta=1.2,
+        scalingRatio=2.0,
+        strongGravityMode=False,
+        gravity=1.0,
+        verbose=False
+    )
+
+    positions = forceatlas2.forceatlas2_networkx_layout(graph)
+
+    print('drawing')
+
+    cmap = cm.get_cmap('winter', max(partition.values()) + 1)
+
+    degrees = dict(graph.degree)
+    max_degree = max(degrees.values())
+    size_multipliers = {i: degrees[i] / max_degree for i in positions}
+
+    # Generate node sizes
+    node_size = [
+        max(max_node_size * size_multipliers[i], min_node_size)
+        for i in positions
+    ]
+
+    # Draw the nodes
+    nx.draw_networkx_nodes(
+        graph,
+        positions,
+        partition.keys(),
+        node_size=node_size,
+        cmap=cmap,
+        node_color=list(partition.values()),
+        alpha=1
+    )
+
+    # Draw the edges with a bezier curve
+    curves = curved_edges(graph, positions)
+    # Assign a colour to each edge, based on the community of the source node.
+    edge_color =  [cmap(partition[a]) for a, _ in graph.edges]
+    edge_lines = LineCollection(curves, color=edge_color, cmap=cmap, alpha=0.05, linewidths=1)
+    plt.gca().add_collection(edge_lines)
+
+    # Draw node labels (words)
+    for i, (x, y) in positions.items():
+        # The size of the label is proportional to the degree of the node.
+        fontsize = max(max_font_size * size_multipliers[i], min_font_size)
+        plt.text(x, y, embeddings.words[i], fontsize=fontsize, ha='center', va='center')
+
+    plt.axis('off')
+    plt.show()
+
+
 embeddings = WordEmbeddings(
     'output/word2vec/00001-shakespeare/proj_weights.npy',
     'output/word2vec/00001-shakespeare/vocab.txt'
 )
-G = build_k_hop_graph(embeddings, 'juliet', 2, alpha=0.89)
-# compute the best partition
-print('computing best partition')
-partition = community_louvain.best_partition(G)
 
-forceatlas2 = ForceAtlas2(
-                        # Behavior alternatives
-                        outboundAttractionDistribution=True,  # Dissuade hubs
-                        edgeWeightInfluence=1.0,
-                        # Performance
-                        jitterTolerance=1.0,  # Tolerance
-                        barnesHutOptimize=True,
-                        barnesHutTheta=1.2,
-                        # Tuning
-                        scalingRatio=2.0,
-                        strongGravityMode=False,
-                        gravity=1.0,
-                        verbose=True)
-
-print('computing forceatlas2 layout')
-positions = forceatlas2.forceatlas2_networkx_layout(G, pos=None, iterations=2000)
-
-print('drawing')
-cmap = cm.get_cmap('tab20b', max(partition.values()) + 1)
-edge_color =  [cmap(partition[a]) for a, b in G.edges]
-
-curves = curved_edges(G, positions)
-lc = LineCollection(curves, color=edge_color, cmap=cmap, alpha=0.25, linewidths=1)
-
-nx.draw_networkx_nodes(G, positions, partition.keys(), node_size=20, cmap=cmap, node_color=list(partition.values()), alpha=1)
-plt.gca().add_collection(lc)
-# nx.draw_networkx_edges(G, positions, edge_cmap=cmap, edge_color=edge_color, alpha=0.05)
-plt.axis('off')
-plt.show()
+draw_k_hop_graph(embeddings, 'juliet', 3, alpha=0.88)
