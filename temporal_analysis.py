2b75b0d8845027951b9f68b5e234841b0574f8cd
GalacticGlum <nerftactic09@gmail.com>
2020-12-14 00:50:16 -0500

    Modified temporal_analysis.py

--- temporal_analysis.py
+++ temporal_analysis.py
@@ -1,8 +1,13 @@
 """Functionality for performing temporal analysis on a corpus."""
 
 from pathlib import Path
-from datetime import date
+from datetime import date, datetime
 from typing import Optional, Union, Tuple, List
+
+import tqdm
+import numpy as np
+import tensorflow as tf
+import tensorflow_text as text
 import matplotlib.pyplot as plt
 
 from utils import list_join
@@ -76,35 +81,133 @@ def plot_frequency_over_time(temporal_tokenizers: List[Tuple[date, Tokenizer]],
     plt.ylabel(y_label)
 
 
-def
+def get_sentiment_over_time(temporal_tweets: List[Tuple[datetime, List[str]]],
+                            model: Optional[object] = None, restore_dir: Optional[Path] = None,
+                            classes: List[int] = [-1, 0, 2, 1],
+                            stat_func: Optional[callable] = None,
+                            show_progress_bar: Optional[bool] = True) -> List[Tuple[date, float]]:
+    """Compute the climate change sentiment of the given tweets over time.
+    Requires one of model or restore_dir.
+
+    Args:
+        model: A loaded climate change sentiment classification model.
+        restore_dir: The checkpoint to load for the climate change sentiment classification model.
+        temporal_tweets: A list of 2-tuples of tweet collections, where the first element is the
+            datetime of the tweets, and the second element is a list of tweet texts.
+
+            For every 'bucket' of tweets, a statitic about sentiment score is calculated,
+            and then plotted. For example, the mean or max sentiment score in the list of tweeets.
+        stat_func: A function which takes in an array-like of sentiment scores, and their
+            probability as given by the text classification model, and then outputs a scalar
+            value describing the sentiment scores (i.e. a summary statistic).
+
+            Defaults to the mean sentiment score as a weighted sum of their probabilities:
+            s = dot(S, P) / n, where S is a vector of the sentiment scores, P is a vector
+            of their probabilities, and n = |S| = |P| is the dimensionality of the vectors.
+        classes: A mapping from class label (output of the model) to a sentiment class (e.g. 1, 0, -1
+            for positive, netural, and negative respectively). Defaults to [-1, 0, 2, 1] which are the
+            sentiment values for the climate change sentiment dataset.
+        show_progress_bar: Whether to show a progress bar while the jobs run.
+    """
+    def _weighted_mean_score(sentiments: tf.Tensor, probabilities: tf.Tensor):
+        """Compute the weighted mean sentiment score.
+
+        Args:
+            sentiments: An int tensor of sentiment classes predicted by the model.
+            probabilities: A float tensor of the probability of each class (i.e. model confidence).
+
+        Preconditions:
+            - sentiments.shape == probabilities.shape
+        """
+        return np.dot(sentiments, probabilities) / sentiments.shape[-1]
+
+    # Set stat_func to default if not specified
+    stat_func = _weighted_mean_score or stat_func
+
+    # Load sentiment classification model (if one not provided)
+    if model is None and restore_dir is not None:
+        model = tf.saved_model.load(restore_dir)
+    elif model is None and restore_dir is None:
+        raise ValueError('Requires one of model or restore_dir.')
+
+    results = []
+    class_tensor = tf.convert_to_tensor(classes, dtype=tf.float32)
+    for timestamp, tweets in tqdm.tqdm(temporal_tweets, disable=not show_progress_bar):
+        outputs = tf.sigmoid(model(tf.constant(tweets)))
+        # class_matrix = tf.reshape(tf.tile(class_tensor, [outputs.shape[0]]), (outputs.shape[0], class_tensor.shape[0]))
+        # yi = tf.reduce_mean(tf.reduce_sum(tf.multiply(outputs, class_matrix), 1)).numpy()
+        sentiments = tf.gather(classes, tf.argmax(outputs, axis=1))
+        probabilities = tf.reduce_max(outputs, axis=1)
+
+        yi = stat_func(sentiments, probabilities)
+        print(timestamp, yi)
+        results.append((timestamp, yi))
+    return results
+
+# if __name__ == '__main__':
+#     from dateutil.parser import parse
+#     paths = Path('./output/word2vec').glob('00001-20*')
+#     # temporal_embeddings = []
+#     temporal_tokenizers = []
+#     for path in paths:
+#         path_name = path.stem
+#         path_name = path_name[path_name.find('-') + 1:path_name.find('_p')].replace('_', '-')
+
+#         path_date = parse(path_name, fuzzy=True).replace(day=1)
+
+#         # Load the embeddings
+#         # embeddings = WordEmbeddings(
+#         #     path / 'proj_weights.npy',
+#         #     path / 'vocab.txt',
+#         #     suffix_tree=False,
+#         #     nearest_neighbours=False
+#         # )
+
+#         # Load the tokenizer
+#         tokenizer = Tokenizer()
+#         tokenizer.load(path / 'tokenizer.json')
+
+#         # temporal_embeddings.append((path_date.date(), embeddings))
+#         temporal_tokenizers.append((path_date.date(), tokenizer))
+
+#     # plot_similarity_over_time(temporal_embeddings, 'climate', 'homelessness')
+#     plot_frequency_over_time(temporal_tokenizers, ['climate_change', 'global_warming', 'disaster'], proportion=True)
+#     plt.show()
+
+
+def random_sampler(filename, k):
+    sample = []
+    with open(filename, 'rb') as f:
+        f.seek(0, 2)
+        filesize = f.tell()
+
+        random_set = sorted(random.sample(range(filesize), k))
+
+        for i in range(k):
+            f.seek(random_set[i])
+            # Skip current line (because we might be in the middle of a line)
+            f.readline()
+            # Append the next line to the sample set
+            sample.append(f.readline().rstrip())
+
+    return sample
 
 
 if __name__ == '__main__':
     from dateutil.parser import parse
-    paths = Path('./output/word2vec').glob('00001-20*')
-    # temporal_embeddings = []
-    temporal_tokenizers = []
-    for path in paths:
-        path_name = path.stem
-        path_name = path_name[path_name.find('-') + 1:path_name.find('_p')].replace('_', '-')
+    import random
+    files = Path('./data/twitter/').glob('20*.txt')
+    temporal_tweets = []
+    for file in files:
+        print(file)
+        filename = file.stem
+        filename = filename[filename.find('-') + 1:filename.find('_p')].replace('_', '-')
+        file_date = parse(filename, fuzzy=True).replace(day=1)
 
-        path_date = parse(path_name, fuzzy=True).replace(day=1)
+        # Get tweets (1K per month to run sentiment analysis)
+        tweets = random_sampler(file, k=1000)
+        temporal_tweets.append((file_date, tweets))
 
-        # Load the embeddings
-        # embeddings = WordEmbeddings(
-        #     path / 'proj_weights.npy',
-        #     path / 'vocab.txt',
-        #     suffix_tree=False,
-        #     nearest_neighbours=False
-        # )
-
-        # Load the tokenizer
-        tokenizer = Tokenizer()
-        tokenizer.load(path / 'tokenizer.json')
-
-        # temporal_embeddings.append((path_date.date(), embeddings))
-        temporal_tokenizers.append((path_date.date(), tokenizer))
-
-    # plot_similarity_over_time(temporal_embeddings, 'climate', 'homelessness')
-    plot_frequency_over_time(temporal_tokenizers, ['climate_change', 'global_warming', 'disaster'], proportion=True)
-    plt.show()
+    model = tf.saved_model.load('output/classifier/00003-climate_change_sentiment/model_final')
+    x = get_sentiment_over_time(temporal_tweets, model=model)
+    print(x)
