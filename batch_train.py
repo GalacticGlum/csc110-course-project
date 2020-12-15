37cd3bd826a14e4a6a4d8bf9ba6208c93b19c9ba
GalacticGlum <nerftactic09@gmail.com>
2020-12-14 22:51:57 -0500

    Removed batch_train.py

--- batch_train.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from pathlib import Path
-
-for file in Path('./data/twitter/2020/p100/').glob('*.txt'):
-    print(f'Training on {file}')
-    subprocess.run([
-        'python', 'train_word2vec.py',
-        str(file), '-e', '1', '--min-word-frequency', '10'
-    ])
